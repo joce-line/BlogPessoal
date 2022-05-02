c0b607986e24fb649ae07a67c577b04bb63a7081
joce-line <joceline.gutierrez@hotmail.com>
2022-05-02 14:50:56 -0300

    @joce-line [Task: Modificação Interface IPost] {02/05/2022 14:46}

--- BlogPessoal/src/repositories/IPost.cs
+++ BlogPessoal/src/repositories/IPost.cs
@@ -17,7 +17,6 @@ namespace BlogPessoal.src.repositories
         void DeletePost(int id);
         PostModel GetPostById(int id);
         List<PostModel> GetAllPosts();
-        List<PostModel> GetPostsByTitle(string title);
-        List<PostModel> GetPostsByDescription(string description);
+        List<PostModel> GetPostBySearch(string title, string descriptionTheme, string nameCreator);
     }
 }
